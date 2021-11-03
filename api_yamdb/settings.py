9154657455931312c4d3263c449accb876ba6903
mitya888 <dbryndin@inbox.ru>
2021-11-03 21:00:53 +0300

    env-2

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,9 +1,9 @@
 import os
 
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
+# import environ
+#
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
