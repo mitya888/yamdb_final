6581cc1f9d860839d5290d586aa51175728d8b9d
mitya888 <dbryndin@inbox.ru>
2021-11-07 09:00:53 +0300

    after slack - 2

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -110,6 +111,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
